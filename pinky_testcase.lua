43c3930f17237e319698db027be046b9e1f0a255
Jaime Fournier <jaimef@linbsd.org>
2013-05-06 16:20:01 -0700

    more tests

--- pinky_testcase.lua
+++ pinky_testcase.lua
@@ -7,6 +7,9 @@ function test_success()
    assert_false(false, "this test never fails")
 end
 
-function test_exec_command()
-   assert_true(pinky.exec_command("ls"))
+function test_exec_command_nxfile()
+   assert_equal('Error: /usr/bin/some_binary_not_there could not be found',pinky.exec_command("/usr/bin/some_binary_not_there"), "Test exec_command on nxfile")
+   assert_equal("y" ,pinky.exec_command("/usr/bin/yes | head -n 1"), "Test exec_command on yes cmd")
+
+
 end
