6f32b110ea3634275af9e149f0849c4ea581ec56
Jaime Fournier <jaimef@linbsd.org>
2013-05-14 16:40:56 -0700

    Fix PINKY_HOME to be a project dir and not my home dir. </derp>

--- pinky.lua
+++ pinky.lua
@@ -76,7 +76,7 @@ end
 
 function dispatch(uri)
    local uri = split(uri,"/")
-   local PINKY_HOME = "/home/jaimef/pinky"
+   local PINKY_HOME = "/data/pinky-server/vendor/projects/pinky/"
    local custom_lib = uri[2]
    -- local custom_lib = PINKY_HOME .. "/" .. uri[2] .. ".lua"
    local short_uri = ""
