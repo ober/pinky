2d8f59731d63cb4dcf71f4148f5ad7a730b605df
Pinky <ubuntu@ip-10-140-2-184.ec2.internal>
2013-05-20 19:45:17 +0000

    Add new ping.lua

--- ping.lua
+++ ping.lua
@@ -7,7 +7,7 @@ function pinky_main(uri)
    local args = p.split(uri,"/")
    -- Arguments:
    -- 0: /port we return usage
-   return report_ping()
+   return report_ping(args[1])
 end
 
 function report_ping(host)
@@ -16,5 +16,28 @@ function report_ping(host)
    local out = p.exec_command("/bin/ping -c 1 -w 1 " .. host, {1,2,3,4,5}, 6, " +",true)
    out = { data = out }
    out.status = { value = "OK", error = "" }
+   return json.encode(out)
+end
+
+function report_ping(host)
+   local cmd = "/bin/ping -n -c 1 -w 1 " .. host .. " 2>&1"
+   local ping = p.exec_command(cmd)
+   local unknown = 'ping: unknown host'
+   local failure = '100%% packet loss,'
+   local success = ' 0%% packet loss,'
+   local out = { data = { ping }, status = { value = "", error = ""} }
+   if ping then
+      if string.find(ping,failure) then
+         out.status.value,out.status.error  = "FAIL", failure
+      elseif string.find(ping,success) then
+         out.status.value = "OK"
+      elseif string.find(ping,unknown) then
+         out.status.value,out.status.error  = "FAIL", unknown
+      else
+         out.status.value,out.status.error  = "FAIL", "fall through"
+      end
+   else
+      out.status.value,out.status.error  = "FAIL", "ping returned nil"
+   end
    return json.encode(out)
 end
