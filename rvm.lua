6c4aacf148981a08f44cbcdd5461eeba2ca74825
Jaime Fournier <jaimef@linbsd.org>
2013-05-22 14:58:37 -0700

    we have gemset lists working now

--- rvm.lua
+++ rvm.lua
@@ -11,27 +11,43 @@ function pinky_main(uri)
    -- 2: /rvm/version/bundler list bundler version
 
    if #args == 0 then
-      return rvm_list_rubies()
+      return json.encode(rvm_list_rubies())
    elseif #args == 1 then
-      return rvm_gem_list(args[1])
+      return json.encode(rvm_gem_list(args[1]))
    elseif #args == 2 then
-      return rvm_bundler_info(args[1])
+      return json.encode(rvm_bundler_info(args[1]))
    end
+end
+
+function get_rvms()
+   local home = p.get_home()
+   local rvms = {
+      "/usr/local/rvm/bin/rvm",
+      home .. "/.rvm/bin/rvm"
+   }
+   return p.find_first_file(rvms)
 end
 
 function rvm_list_rubies()
    -- return a list of all rubies
-   return json.encode(p.lsdir(RVM_PATH .. "/wrappers/"))
+   rvm = get_rvms()
+   cmd = "/usr/bin/env " .. rvm .. " list strings"
+   return p.exec_command(cmd,{1},1," +", true)
 end
 
 function rvm_gem_list(ruby)
    -- return gem list of the ruby provided
-   cmd = "/usr/bin/env rvm list"
-   return json.encode(p.exec_command(cmd))
+   rvm = get_rvms()
+   rubies = rvm_list_rubies()
+   if rubies[ruby] then
+      cmd = "/usr/bin/env " .. rvm .. " " .. rubies[ruby][1] .. " do gem list"
+      return p.exec_command(cmd,{2},1," +", true)
+   end
+   return "Could not find " .. ruby .. " in ruby list"
 end
 
 function rvm_bundler_info(ruby)
    -- Return the bundler specific information on this bundler
    cmd = RVM_PATH .. "/gems/" .. ruby .. "@global/bin/bundle info"
-   return json.encode(p.exec_command(cmd,{1,2,3,4,5,6}, 1, " +", true))
+   return p.exec_command(cmd,{1,2,3,4,5,6}, 1, " +", true)
 end
