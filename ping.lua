8a9359e38a2a2b3b2c8a1407c5b39de7001ca7b2
Jaime Fournier <jaimef@linbsd.org>
2013-05-24 14:43:29 -0700

    fix osx case

--- ping.lua
+++ ping.lua
@@ -7,37 +7,46 @@ function pinky_main(uri)
    local args = p.split(uri,"/")
    -- Arguments:
    -- 0: /port we return usage
-   return report_ping(args[1])
-end
-
-function report_ping(host)
-   -- Ping report.
-   -- Return the output of ping(1)
-   local out = p.exec_command("/bin/ping -c 1 -w 1 " .. host, {1,2,3,4,5}, 6, " +",true)
-   out = { data = out }
-   out.status = { value = "OK", error = "" }
-   return json.encode(out)
-end
-
-function report_ping(host)
-   local cmd = "/bin/ping -n -c 1 -w 1 " .. host .. " 2>&1"
-   local ping = p.exec_command(cmd)
-   local unknown = 'ping: unknown host'
-   local failure = '100%% packet loss,'
-   local success = ' 0%% packet loss,'
-   local out = { data = { ping }, status = { value = "", error = ""} }
-   if ping then
-      if string.find(ping,failure) then
-         out.status.value,out.status.error  = "FAIL", failure
-      elseif string.find(ping,success) then
-         out.status.value = "OK"
-      elseif string.find(ping,unknown) then
-         out.status.value,out.status.error  = "FAIL", unknown
-      else
-         out.status.value,out.status.error  = "FAIL", "fall through"
-      end
+   local ip = p.get_ip(tostring(args[1]))
+   if ip then
+      return json.encode(report_ping(ip))
    else
-      out.status.value,out.status.error  = "FAIL", "ping returned nil"
+      return json.encode({ data = {}, status = { value = "FAIL", error = "Could not resolve the host provided " }})
    end
-   return json.encode(out)
+end
+
+function report_ping(host)
+
+   local host_os = p.get_os()
+   if host_os == "Darwin" then
+      ping = "/sbin/ping -n -c 1 -W 1 " .. host
+   elseif host_os == "Linux" then
+      ping = "/sbin/ping -n -c 1 -w 1 " .. host
+   end
+
+   if ping then
+      local ping_out = p.exec_command(ping) -- ,nil,nil," +", true)
+      local unknown = 'ping: unknown host'
+      local failure = '100%% packet loss,'
+      local success = ' 0%% packet loss,'
+      local success_osx = ' 0.0%% packet loss,'
+      local out = { data = { ping_out }, status = { value = "", error = ""} }
+
+      if ping_out then
+         if string.find(ping_out,failure) then
+            out.status.value,out.status.error  = "FAIL", failure
+         elseif string.find(ping_out,success) then
+            out.status.value = "OK"
+         elseif string.find(ping_out,success_osx) then
+            out.status.value = "OK"
+         elseif string.find(ping_out,unknown) then
+            out.status.value,out.status.error  = "FAIL", unknown
+         else
+            out.status.value,out.status.error  = "FAIL", "fall through"
+         end
+      else
+         out.status.value,out.status.error  = "FAIL", "ping returned nil"
+      end
+      return out
+   end
 end
