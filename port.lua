4a2677f60c4ccdfc9722dc6aa27eba4d86e9399e
Jaime Fournier <jaimef@linbsd.org>
2013-05-24 15:02:54 -0700

    fix user input to be untainted

--- port.lua
+++ port.lua
@@ -22,13 +22,25 @@ function pinky_main(uri)
    if #args < 2 then
       return json.encode(usage)
    elseif #args == 2 then
-      return report_port(args[1],args[2])
+      local ip = p.get_ip(tostring(args[1]))
+      local port = tonumber(args[2])
+
+      if ip and port then
+         return json.encode(report_port(ip,port))
+      else
+         return json.encode({ data = {}, status = { value = "FAIL", error = "Could not resolve the host provided or the port" }})
+      end
    end
 end
 
 function report_port(host,port)
-   local cmd = "/bin/nc -v -z -w 1 " .. host .. " " .. port .. " 2>&1"
+
+   local cmd = "/usr/bin/env nc -v -z -w 1 " .. host .. " " .. port .. " 2>&1"
    local nc = p.exec_command(cmd)
+   if type(nc) == "table" then
+      return p.print_table(nc)
+   end
+
    local success = "succeeded!"
    local failure = "failed: Connection refused"
    local out = { data = { nc }, status = { value = "", error = ""} }
