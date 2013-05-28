f83820ada3838de015eee0315c88980dd74fcd17
Jaime Fournier <jaimef@linbsd.org>
2013-05-28 15:38:50 -0700

    env not eval

--- passenger.lua
+++ passenger.lua
@@ -6,7 +6,7 @@ local pinky_main;
 
 function pinky_main()
    local out = {
-      data = p.xml_find_tags(p.exec_command("/usr/bin/eval passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
+      data = p.xml_find_tags(p.exec_command("/usr/bin/env passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
       status = { value = "OK", error = "" }}
    return json.encode(out)
 end
