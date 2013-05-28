7720f8bf4924cd118a9a455cae14be0aa1855da6
Jaime Fournier <jaimef@linbsd.org>
2013-05-28 15:29:11 -0700

    more cleanup

--- passenger.lua
+++ passenger.lua
@@ -5,14 +5,9 @@ local json = require 'cjson'
 local pinky_main;
 
 function pinky_main()
-   xmldata = p.exec_command("/usr/bin/eval passenger-status --show=xml")
    local out = {
-      data = p.xml_find_tags(,{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
-      status = {
-         value = "OK";
-         error = "";
-      }
-   }
+      data = p.xml_find_tags(p.exec_command("/usr/bin/eval passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
+      status = { value = "OK", error = "" }}
    return json.encode(out)
 end
 
