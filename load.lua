d3aa0305d77fb49f36c6f11ffe1481a90a1eb8e5
Jaime Fournier <jaimef@linbsd.org>
2013-05-28 11:06:25 -0700

    add load pinky

---
+++ load.lua
@@ -0,0 +1,21 @@
+local p = require 'pinky'
+local json = require 'cjson'
+
+local function pinky_main(uri)
+   local one,five,ten =  p.exec_command("/usr/bin/uptime"):match("(%d+\.%d+)\ +(%d+\.%d+)\ +(%d+\.%d+)$")
+    -- 10:57  up 13 days,  1:14, 1 user, load averages: 0.64 0.65 0.70
+
+   return json.encode({
+                         data = {
+                            one = one;
+                            five = five;
+                            ten = ten;
+                         },
+                         stats = {
+                            value = "OK";
+                            error = "";
+                         }
+                      })
+end
+
+return { pinky_main = pinky_main }
