b64ba679fa3d8881cd26ffc2b9023f732a0b30bd
Jaime Fournier <jaimef@linbsd.org>
2013-05-28 15:28:11 -0700

    Fix functionr eturn

--- passenger.lua
+++ passenger.lua
@@ -4,16 +4,16 @@ local json = require 'cjson'
 
 local pinky_main;
 
- function pinky_main()
-    xmldata = p.exec_command("/usr/bin/eval passenger-status --show=xml")
-    local out = {
+function pinky_main()
+   xmldata = p.exec_command("/usr/bin/eval passenger-status --show=xml")
+   local out = {
       data = p.xml_find_tags(,{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
       status = {
          value = "OK";
          error = "";
       }
    }
-   print(json.encode(out))
+   return json.encode(out)
 end
 
 return { pinky_main = pinky_main }
