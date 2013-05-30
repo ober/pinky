2f9973c6eebb93a4537ddc8cdb28d308f571a846
Jaime Fournier <jaimef@linbsd.org>
2013-05-29 17:25:36 -0700

    add formatting for linux uptime

--- load.lua
+++ load.lua
@@ -2,7 +2,7 @@ local p = require 'pinky'
 local json = require 'cjson'
 
 local function pinky_main(uri)
-   local one,five,ten =  p.exec_command("/usr/bin/uptime"):match("(%d+%.%d+) +(%d+%.%d+) +(%d+%.%d+)$")
+   local one,five,ten =  p.exec_command("/usr/bin/uptime"):match("(%d+%.%d+),? +(%d+%.%d+),? +(%d+%.%d+)$")
    -- 10:57  up 13 days,  1:14, 1 user, load averages: 0.64 0.65 0.70
    return json.encode({
                          data = {
