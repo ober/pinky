47a2ed6b1121022963bc68c118030698554ba7f5
Jaime Fournier <jaimef@linbsd.org>
2013-05-31 16:49:42 -0700

    remove dumb comma

--- passenger.lua
+++ passenger.lua
@@ -6,7 +6,7 @@ local pstatus = { data = {}, status = { value = "OK", error = ""}}
 local pinky_main;
 
 function pinky_main()
-   pstatus.data = p.xml_find_tags(p.exec_command("/usr/bin/env passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } ),
+   pstatus.data = p.xml_find_tags(p.exec_command("/usr/bin/env passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } )
    return json.encode(pstatus)
 end
 
