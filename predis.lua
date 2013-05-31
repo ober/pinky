c3d05c3723e6eabe306eface4d14647b2c4a4d4a
Jaime Fournier <jaimef@linbsd.org>
2013-05-31 11:27:18 -0700

    We handle recursion!

--- predis.lua
+++ predis.lua
@@ -3,11 +3,20 @@ local p = require 'pinky'
 local json = require 'cjson'
 local redis = require "redis"
 
+local handle_list;
+local handle_none;
+local handle_set;
+local handle_string;
 local pinky_main;
-local usage;
+local process_redis;
 local redis_check;
+local redis_connect;
 local redis_getdata;
 local resque_check;
+local singularize;
+local usage;
+
+
 
 local pstatus = {
    data = {};
@@ -30,7 +39,6 @@ end
 local function pinky_main(uri)
    -- This function is the entry point.
    local args = p.split(uri,"/")
-   -- Arguments:
 
    if #args == 0 then -- Check if Redis is up return appropriate info.
       usage()
@@ -38,9 +46,15 @@ local function pinky_main(uri)
       if args[1] == "up" then
          redis_check()
       elseif args[1] == "workers" then
-         resque_getdata("resque:workers","resque:worker:")
+         process_redis("resque:workers")
       elseif args[1] == "queues" then
-         resque_getdata("resque:queues","resque:queue:")
+         process_redis("resque:queues")
+      elseif args[1] == "failed" then
+         process_redis("resque:failed")
+      elseif args[1] == "loners" then
+         process_redis("resque:loners")
+      elseif args[1] == "stat" then
+         process_redis("resque:stat")
       else
          usage()
       end
@@ -60,32 +74,56 @@ function redis_check()
    pstatus.status.value,pstatus.data = "OK", response
 end
 
-function resque_getdata(set,short)
-   set = set or 'resque:workers'
-   local client = redis_connect()
-   print("smember type is : " .. client:type(set))
-   for i, v in ipairs(client:smembers(set)) do
-      v = short .. v
-      if v then
-         local type = client:type(v)
-         if type == "list" then
-            local len = client:llen(v)
-            if len then
-               for l=1,len,1 do
-                  pstatus.data[tostring(v)] = tostring(client:lindex(v,l - 1))
-               end
-            else
-               print("Empty list")
-            end
-         elseif type == "none" then
-            print("got v:" .. v .. " type:" .. type)
-            pstatus.data[tostring(v)] = tostring(client:get(v))
-         elseif type == "string" then
-            pstatus.data[tostring(v)] = tostring(client:get(v))
-         else
-            print("Unknown type:", client:type(v))
-         end
+function handle_list(list,client,parent)
+   print("handle_list: list:" .. list .. " parent:" .. parent)
+   parent = parent or ""
+   local len = client:llen(parent .. list)
+   if len then
+      len = len > 2000 and 2000 or len
+      for l=1,len,1 do
+         pstatus.data[parent .. list .. l] = client:lindex(parent .. list,l - 1)
       end
+   end
+end
+
+function handle_set(set,client,parent)
+   parent = parent or ""
+   for i, v in ipairs(client:smembers(set)) do
+      pstatus.data[parent .. set] = {}
+      v = singularize(set) .. ":" .. v
+      if v then
+         process_redis(v,client,parent)
+      end
+   end
+end
+
+function handle_string(string,client,parent)
+   pstatus.data[parent .. string] = client:get(parent .. string)
+end
+
+function handle_none(none,client,parent)
+   pstatus.data[parent .. none] = client:get(parent .. none)
+end
+
+function singularize(name)
+   return string.sub(name,1,-2)
+end
+
+function process_redis(something,client,parent)
+   local parent = parent or ""
+   local client = client or redis_connect()
+   local type = client:type(something)
+
+   if type == "set" then
+      handle_set(something,client,parent)
+   elseif type == "list" then
+      handle_list(something,client,parent)
+   elseif type == "none" then
+      handle_none(something,client,parent)
+   elseif type == "string" then
+      handle_string(something,client,parent)
+   else
+      print("Unknown type:" .. type)
    end
 end
 
