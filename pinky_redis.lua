6087ffc108f8dbbaf823e3c5442f8eed5632cb97
Jaime Fournier <jaimef@linbsd.org>
2013-06-04 15:19:49 -0700

    fun stuff

--- pinky_redis.lua
+++ pinky_redis.lua
@@ -15,16 +15,15 @@ local singularize;
 local usage;
 
 local function usage()
-   ps.status.value = "FAIL"
-   ps.status.error = [[
+   return [[
 Check Redis:
 Valid URLS:
-/pinky/predis/up      -- Check if redis is up locally.
-/pinky/predis/workers  -- Return resque worker information.
-/pinky/predis/queues  -- Return resque queues information.
-/pinky/predis/failed  -- Return resque failed job information. (max 2k)
-/pinky/predis/stat  -- Return resque stat entries
-/pinky/predis/loners  -- Return resque loners job entries
+/pinky/redis/up      -- Check if redis is up locally.
+/pinky/redis/workers  -- Return resque worker information.
+/pinky/redis/queues  -- Return resque queues information.
+/pinky/redis/failed  -- Return resque failed job information. (max 2k)
+/pinky/redis/stat  -- Return resque stat entries
+/pinky/redis/loners  -- Return resque loners job entries
 ]];
 end
 
@@ -33,22 +32,22 @@ local function pinky_main(uri,ps)
    local args = p.split(uri,"/")
 
    if #args == 0 then -- Check if Redis is up return appropriate info.
-      usage()
+      ps = p.do_error(usage(),ps)
    else
       if args[1] == "up" then
-         redis_check()
+         ps.data = redis_check()
       elseif args[1] == "workers" then
-         process_redis("resque:workers")
+         ps = process_redis("resque:workers",nil,nil,ps)
       elseif args[1] == "queues" then
-         process_redis("resque:queues")
+         ps = process_redis("resque:queues",nil,nil,ps)
       elseif args[1] == "failed" then
-         process_redis("resque:failed")
+         ps = process_redis("resque:failed",nil,nil,ps)
       elseif args[1] == "loners" then
-         process_redis("resque:loners")
+         ps = process_redis("resque:loners",nil,nil,ps)
       elseif args[1] == "stat" then
-         process_redis("resque:stat")
+         ps = process_redis("resque:stat",nil,nil,ps)
       else
-         usage()
+         ps.status.value,ps.status.error = "FAIL", usage()
       end
    end
    return ps
@@ -57,28 +56,32 @@ end
 function redis_connect(ip,port)
    local ip = ip or '127.0.0.1'
    local port = port or 6379
-   return redis.connect(ip,port)
+   local client = redis.connect(ip,port)
+   return client,"Could not connect to the redis db"
 end
 
 function redis_check()
-   local client = redis_connect()
-   local response = client:ping()
-   ps.status.value,ps.data = "OK", response
+   local client,err = redis_connect()
+   if err then
+      local response = client:ping()
+   else
+      return response
+   end
 end
 
-function handle_list(list,client,parent)
+function handle_list(list,client,parent,ps)
    print("handle_list: list:" .. list .. " parent:" .. parent)
    parent = parent or ""
    local len = client:llen(parent .. list)
    if len then
       len = len > 2000 and 2000 or len
       for l=1,len,1 do
-         ps.data[parent .. list .. l] = client:lindex(parent .. list,l - 1)
+         ps.data[parent .. list .. l] = client:lindex(parent .. list, l - 1)
       end
    end
 end
 
-function handle_set(set,client,parent)
+function handle_set(set,client,parent,ps)
    parent = parent or ""
    for i, v in ipairs(client:smembers(set)) do
       ps.data[parent .. set] = {}
@@ -87,33 +90,36 @@ function handle_set(set,client,parent)
          process_redis(v,client,parent)
       end
    end
+   return ps
 end
 
-function handle_string(string,client,parent)
+function handle_string(string,client,parent,ps)
    ps.data[parent .. string] = client:get(parent .. string)
+   return ps
 end
 
-function handle_none(none,client,parent)
+function handle_none(none,client,parent,ps)
    ps.data[parent .. none] = client:get(parent .. none)
+   return ps
 end
 
 function singularize(name)
    return string.sub(name,1,-2)
 end
 
-function process_redis(something,client,parent)
+function process_redis(something,client,parent,ps)
    local parent = parent or ""
    local client = client or redis_connect()
    local type = client:type(something)
 
    if type == "set" then
-      handle_set(something,client,parent)
+      ps = handle_set(something,client,parent,ps)
    elseif type == "list" then
-      handle_list(something,client,parent)
+      ps = handle_list(something,client,parent,ps)
    elseif type == "none" then
-      handle_none(something,client,parent)
+      ps = handle_none(something,client,parent,ps)
    elseif type == "string" then
-      handle_string(something,client,parent)
+      ps = handle_string(something,client,parent,ps)
    else
       print("Unknown type:" .. type)
    end
