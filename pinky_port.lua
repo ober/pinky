7ba72d6a6cef1f526a8aeb48fcb52f3ff6b5ddf7
Jaime Fournier <jaimef@linbsd.org>
2013-06-04 14:32:07 -0700

    Fix port to not do recursion...

--- pinky_port.lua
+++ pinky_port.lua
@@ -3,6 +3,8 @@ local p = require 'pinky'
 local success = "succeeded!"
 local failure = "failed: Connection refused"
 
+local pinky_main
+
 local usage = {
    status = {
       value = "FAIL";
@@ -14,34 +16,35 @@ Valid URLS:
    }
 }
 
-local function pinky_main(uri,ps)
+function pinky_main(uri,ps)
    -- This function is the entry point.
    -- local ip, port = unpack(p.split(uri,"/"))
    ps.ip,ps.port = uri:match("([^/]+)/(%d+)")
 
    if not ps.port then
+      print("no ps.port")
       ps = p.do_error(usage,ps)
-   end
-   ps.ip = p.get_ip(tostring(ps.ip))
-
-   if ps.ip then
-      ps = report_port(ps)
    else
-      ps = p.do_error("Could not resolve the host provided or the port",ps)
+      ps.ip = p.get_ip(tostring(ps.ip))
+      if ps.ip then
+         ps,ps.status.error = report_port(ps)
+      else
+         ps.data,ps.status.error = "FAIL","Could not resolve the host provided or the port"
+      end
    end
-
+   -- print("AAA" .. print_table(ps) .. "BBB")
    return ps
 end
 
 function report_port(ps)
-   local host = ps.host
+   local host = ps.ip
    local port = ps.port
    local cmd = "/usr/bin/env nc -v -z -w 1 " .. host .. " " .. port .. " 2>&1"
    local nc = p.exec_command(cmd)
+
    if type(nc) == "table" then
       return p.print_table(nc)
    end
-
    if nc then
       if string.match(nc,failure) then
          ps = p.do_error(failure,ps)
@@ -53,7 +56,7 @@ function report_port(ps)
    else
       ps = p.do_error("nc returned nil", ps)
    end
-   return ps
+   return ps,err
 end
 
 return { pinky_main = pinky_main }
