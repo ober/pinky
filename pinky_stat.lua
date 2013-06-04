2df050999694f517ffd75310e4e18613a8d053a7
Jaime Fournier <jaimef@linbsd.org>
2013-06-04 15:38:37 -0700

    working stat

--- pinky_stat.lua
+++ pinky_stat.lua
@@ -5,26 +5,27 @@ local json = require 'cjson'
 local pinky_main;
 local stat_file;
 
-local pstatus = { data = {}, status = { value = "OK", error = ""}}
-
-function pinky_main(file)
+function pinky_main(file,ps)
    -- This function is the entry point.
    -- Arguments:
    -- The only argument we take is the full path to a single file.
 
-   if file then
-      stat_file(file)
+   if file and file ~= "" then
+      ps = stat_file(file,ps)
+   else
+      ps = p.do_error("Usage: /pinky/stat/some/path/to/a/file", ps)
    end
 
-   return json.encode(pstatus)
+   return ps
 end
 
-function stat_file(file)
-   if file_exists(file) then
-      pstatus.data = lfs.attributes(file)
+function stat_file(file,ps)
+   if p.file_exists(file) then
+      ps.data = lfs.attributes(file)
    else
-      pstatus.status.value,pstatus.status.error = "FAIL", "File does exist!"
+      ps.status.value,ps.status.error = "FAIL", "File does exist!"
    end
+   return ps
 end
 
 return { pinky_main = pinky_main }
