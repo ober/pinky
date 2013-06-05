b227e6ea41caa80f02ff99ecb55f8a854613fa55
Jaime Fournier <jaimef@linbsd.org>
2013-06-05 11:52:19 -0700

    Add error handling to avoid 500's on missing unicorns.

--- pinky_unicorn.lua
+++ pinky_unicorn.lua
@@ -7,22 +7,31 @@ local unicorn_check_version;
 function pinky_main(uri,ps)
    -- This function is the entry point.
    local args = p.split(uri,"/")
-   ps = unicorn_check_version(ps)
+   ps,err = unicorn_check_version(ps)
+
+   if err then
+      ps.status.value,ps.status.error  = "FAIL", err
+   end
+
    return ps
 end
 
 function unicorn_check_version(ps)
    -- get pid Check that the /data/api/var/unicorn.pid
-   ps.data = {
-      current = posix.readlink("/data/api/current");
-      master_wd = posix.readlink("/proc/" .. p.trim(p.read_file("/data/api/var/unicorn.pid")) .. "/cwd");
-   }
-   if ps.data.master_wd == ps.data.current then
-      ps.status.value,ps.status.error = "OK",""
-   else
-      ps.status.value,ps.status.error = "FAIL","Current unicorn master is running from somewhere other than /data/api/current"
-   end
-   return ps
+   local pid,err = p.read_file("/data/api/var/unicorn.pid")
+   if not err then
+      pid = p.trim(pid)
+      ps.data = {
+         current = posix.readlink("/data/api/current");
+         master_wd = posix.readlink("/proc/" ..  pid .. "/cwd");
+      }
+      if ps.data.master_wd == ps.data.current then
+         ps.status.value,ps.status.error = "OK",""
+      else
+         ps.status.value,ps.status.error = "FAIL","Current unicorn master is running from somewhere other than /data/api/current"
+      end
+
+      return ps,err
 end
 
 return { pinky_main = pinky_main }
