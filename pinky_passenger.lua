ab5e0423985b69b72b1112f48290d70c5802d336
Jaime Fournier <jaimef@linbsd.org>
2013-06-05 14:24:49 -0700

    working passenger finally

--- pinky_passenger.lua
+++ pinky_passenger.lua
@@ -4,7 +4,13 @@ local xml = require 'LuaXml'
 local pinky_main;
 
 function pinky_main(uri,ps)
-   ps.data = p.xml_find_tags(p.exec_command("/usr/bin/env passenger-status --show=xml"),{ "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"  } )
+   local cmd_out = p.exec_command("/usr/bin/env passenger-status --show=xml")
+   if cmd_out == "" then
+     ps.status.value,ps.status.error = "FAIL", "Could not find passenger-status"
+   else
+     ps.data = p.xml_find_tags(cmd_out, { "active", "count", "max", "global_queue_size", "app_root", "environment", "cpu", "rss", "pss", "real_memory", "vmsize", "command"})
+   end
+
    return ps
 end
 
