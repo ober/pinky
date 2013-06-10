78161f4275af807c8d33cba9e10641df8fa68075
Jaime Fournier <jaimef@linbsd.org>
2013-06-10 16:25:39 -0700

    fix load as it's 1,5,15, not 1,5,10

--- pinky_load.lua
+++ pinky_load.lua
@@ -3,9 +3,9 @@ local p = require 'pinky'
 local pinky_main;
 
 function pinky_main(uri,ps)
-   local one,five,ten =  p.exec_command("/usr/bin/uptime"):match("(%d+%.%d+),? +(%d+%.%d+),? +(%d+%.%d+)$")
+   local one,five,fifteen =  p.exec_command("/usr/bin/uptime"):match("(%d+%.%d+),? +(%d+%.%d+),? +(%d+%.%d+)$")
    -- 10:57  up 13 days,  1:14, 1 user, load averages: 0.64 0.65 0.70
-   ps.data = { one = one, five = five, ten = ten }
+   ps.data = { one = one, five = five, fifteen = fifteen }
 
    return ps
 end
