25f2478a3b8709ff1d8db3e00799c3a5da1cba5a
Jaime Fournier <jaimef@linbsd.org>
2013-10-04 16:16:04 -0700

    add proper process output

--- pinky_process.lua
+++ pinky_process.lua
@@ -6,7 +6,7 @@ local search_process;
 function pinky_main(uri,ps)
    local args = p.split(uri,"/")
 
-   ps.data = p.exec_command("/bin/ps aux", nil, 11, " +", true)
+   ps.data = p.exec_command("/bin/ps -eo pid= -o ni= -o pri= -o psr= -o pcpu= -o stat= -o rss= -o comm= -o cmd=", {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}, 1, " +", true)
    if #args == 1 then
       search_process(ps,args[1],out)
    end
