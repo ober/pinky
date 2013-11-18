0362f80f051791c13d7d5011c65acd6d76ba455b
jaimef <jaimef@jaimef-MacBookAir.(none)>
2013-11-18 00:52:35 -0800

    Fix net controller

--- pinky_net.lua
+++ pinky_net.lua
@@ -3,7 +3,7 @@ local p = require 'pinky'
 local pinky_main;
 
 function pinky_main(uri,ps)
-   ps.data = p.exec_command("/bin/netstat -an", nil, 1, " +",true)
+   ps.data = p.exec_command("/bin/netstat -tan|grep -E '^udp|^tcp'|cat -n|tr '\t' ' '", {2,3,4,5,6,7}, 1, " +", true)
    return ps
 end
 
